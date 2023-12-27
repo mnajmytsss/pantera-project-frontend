fa0a1294186e16b2432102bf668d4cd69cada8f1
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-27 22:20:10 +0700

    feat: PublicationCard

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 // import ButtonM from './components/Button-M';
 // import ButtonS from './components/Button-S';
 // import CustomTag from './components/Tag';
-// import MultiActionAreaCard from './components/CardHighlight';
-import CardMission from './components/CardMission';
+import MultiActionAreaCard from './components/HighlightCard';
+import CardMission from './components/MissionCard';
+import PublicationCard from './components/PublicationCard'
+import domba from '../src/assets/domba.jpeg'
 
 function App() {
   return (
@@ -14,9 +16,10 @@ function App() {
       {/* <ButtonL label="Discover More" />
       <ButtonM label="Discover More" />
       <ButtonS label="Discover More" />
-      <CustomTag label="Custom Tag" />
-      <MultiActionAreaCard /> */}
+      <CustomTag label="Custom Tag" /> */}
+      <MultiActionAreaCard />
       <CardMission />
+      <PublicationCard imageUrl={domba} title="Muara Gembong: Non-Forested Forest Areas" typography="By definition, forest area is a specific area determined by the Government to preserve its existence as a Permanent Forest. In" buttonLabel="Read More"/>
     </div>
   );
 }
