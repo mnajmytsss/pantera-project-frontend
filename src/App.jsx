eeff89618b263377b48e6ca6e7a1f06167bc0b41
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-27 12:34:23 +0700

    feat: card for mission page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import './App.css'
 // import Navbar from "./components/Nav"
-import ButtonL from "./components/Button-L"
-import ButtonM from './components/Button-M';
-import ButtonS from './components/Button-S';
-import CustomTag from './components/Tag';
-import MultiActionAreaCard from './components/CardHighlight';
+// import ButtonL from "./components/Button-L"
+// import ButtonM from './components/Button-M';
+// import ButtonS from './components/Button-S';
+// import CustomTag from './components/Tag';
+// import MultiActionAreaCard from './components/CardHighlight';
+import CardMission from './components/CardMission';
 
 function App() {
   return (
     <div>
       {/* <Navbar /> */}
-      <ButtonL label="Discover More" />
+      {/* <ButtonL label="Discover More" />
       <ButtonM label="Discover More" />
       <ButtonS label="Discover More" />
       <CustomTag label="Custom Tag" />
-      <MultiActionAreaCard />
+      <MultiActionAreaCard /> */}
+      <CardMission />
     </div>
   );
 }
