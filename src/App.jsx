8f98b328a874506a00d187fda1b8a64157df8226
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-28 19:14:49 +0700

    feat: try out the card components

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import './App.css'
 // import ButtonM from './components/Button-M';
 // import ButtonS from './components/Button-S';
 // import CustomTag from './components/Tag';
-import MultiActionAreaCard from './components/HighlightCard';
+import HighlightCard from './components/HighlightCard';
 import CardMission from './components/MissionCard';
 import PublicationCard from './components/PublicationCard'
-import domba from '../src/assets/domba.jpeg'
+import khdpk from '../src/assets/KHDPK.jpg'
+import OurTeamCard from './components/OurTeamCard';
+import gambar2 from '../src/assets/2.jpg'
+import gambar1 from '../src/assets/1.jpg'
+import gambar4 from '../src/assets/4.jpg'
+import PostsCard from './components/PostsCard';
 
 function App() {
   return (
@@ -17,9 +22,11 @@ function App() {
       <ButtonM label="Discover More" />
       <ButtonS label="Discover More" />
       <CustomTag label="Custom Tag" /> */}
-      <MultiActionAreaCard />
-      <CardMission />
-      <PublicationCard imageUrl={domba} title="Muara Gembong: Non-Forested Forest Areas" typography="By definition, forest area is a specific area determined by the Government to preserve its existence as a Permanent Forest. In" buttonLabel="Read More"/>
+      <PostsCard image={gambar4} postCaption="Scoping for Accelerated Implementation of Social Forestry at KHDPK in Java: Sumedang" customTagLabel="EVENTS, UPCOMING" />
+      <HighlightCard description="Scoping for Accelerated Implementation of Social Forestry at KHDPK in Java: Indramayu" image={gambar4} customTagLabel="Program Snapshot"/>
+      <CardMission image={gambar1} caption="Patera Smallholders Support"/>
+      <PublicationCard imageUrl={khdpk} title="Muara Gembong: Non-Forested Forest Areas" typography="By definition, forest area is a specific area determined by the Government to preserve its existence as a Permanent Forest. In dsfdsdfsdfd dsfsdf dsfsdf sdfsdfsdvsdwedfd ewfwedwedwe fw" buttonLabel="Read More"/>
+      <OurTeamCard name="Rachmatul Rajab Aqsha" jobTitle="Deputy Manager" imageUrl={gambar2}/>
     </div>
   );
 }
