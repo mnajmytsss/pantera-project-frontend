43d137e1c972feebdb6c6efb04c4928421472a54
mnajmytsss <najmytsaqiby22@gmail.com>
2023-12-28 19:19:37 +0700

    fix: bug props on link button

--- src/components/PublicationCard/index.jsx
+++ src/components/PublicationCard/index.jsx
@@ -2,14 +2,14 @@
 import "./PublicationCard.css";
 import ButtonM from "../Button-M";
 
-const PublicationCard = ({ imageUrl, title, typography, buttonLabel }) => {
+const PublicationCard = ({ imageUrl, title, typography, buttonLabel, onButtonClick}) => {
   return (
     <div className="publication-card">
       <img className="image" src={imageUrl} />
       <div className="caption">
         <h3>{title}</h3>
         <p>{typography}</p>
-        <ButtonM label={buttonLabel} />
+        <ButtonM label={buttonLabel} onClick={onButtonClick}/>
       </div>
     </div>
   );
